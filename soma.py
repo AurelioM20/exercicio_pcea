51e226d9b45968bec6bf996a26ea0f6ac5ba4ca3
AurelioM20 <153546989+AurelioM20@users.noreply.github.com>
2025-06-09 23:07:03 -0300

    Update soma.py

--- soma.py
+++ soma.py
@@ -1,2 +1,4 @@
-def soma(e,b):
-    return e+b
+def soma(y,z):
+    return y + z
+    #Essa função soma(y,z) em Python serve para somar dois números que você passa como parâmetros, como y=5 e z=3, e ela retorna o resultado, no caso, 8
+
